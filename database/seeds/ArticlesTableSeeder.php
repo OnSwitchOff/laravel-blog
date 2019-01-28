44c245909276743955b03230849d6e22e5c06dd1
OnSwitchOff <kassov@ukr.net>
2019-01-28 18:21:48 +0200

    final seeder

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -18,17 +18,17 @@ class ArticlesTableSeeder extends Seeder
     public function run()
     {
         factory(Article::class, 1000)->create()->each(function ($article) {
-            $faker=new Faker();
+            $faker=\Faker\Factory::create();;
             $catCount=$faker->numberBetween(1,4);
             $tagCount=$faker->numberBetween(1,10);
             for ($i=0; $i <$catCount; $i++) {
                 $catId=$faker->numberBetween(1,Category::count());
-                $article->categories()->save(Category::where('id',$catId)->get()->make());
+                $article->categories()->save(Category::find($catId));
             }
 
             for ($i=0; $i <$tagCount; $i++) {
                 $tagId=$faker->numberBetween(1,Tag::count());
-                $article->tags()->save(Tag::where('id',$tagId)->get()->make());
+                $article->tags()->save(Tag::find($tagId));
             }
             
         });
